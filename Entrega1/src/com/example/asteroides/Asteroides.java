a4b0224a8172e4898bb2bf12cbdd6f69d538c156
David Dominguez <dominguezcalvo.david@gmail.com>
2013-03-11 18:33:35 +0100

    Curso android

---
+++ Entrega1/src/com/example/asteroides/Asteroides.java
@@ -0,0 +1,91 @@
+package com.example.asteroides;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class Asteroides extends Activity {
+	
+	private Button bAcercaDe;
+	//private Button bTerminar;
+	public static AlmacenPuntuaciones almacen = new AlmacenPuntuacionesArray();
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        bAcercaDe =(Button) findViewById(R.id.button3);
+        bAcercaDe.setOnClickListener(new OnClickListener() {
+                   public void onClick(View view) {
+                        lanzarAcercaDe(null);
+                  }
+
+            });
+        
+     /* Boton por codigo  
+        bTerminar =(Button) findViewById(R.id.button4);
+        bTerminar.setOnClickListener(new OnClickListener() {
+                   public void onClick(View view) {
+                        finish();
+                  }
+
+            });
+            */
+    }
+
+
+/*    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.asteroides, menu);
+        return true;
+    }
+ */
+    public void lanzarAcercaDe(View view){
+        Intent i = new Intent(this, AcercaDe.class);
+              startActivity(i);
+      }
+ 
+    public void lanzarPreferencias(View view){
+        Intent i = new Intent(this, Preferencias.class);
+              startActivity(i);
+      }
+    
+    public void lanzarPuntuaciones(View view) {
+    	Intent i = new Intent(this, Puntuaciones.class);
+    	startActivity(i);
+    	}
+    
+    public void terminar(View view){
+              finish();
+      }
+    
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true; /** true -> el menú ya está visible */
+     }
+    
+     @Override public boolean onOptionsItemSelected(MenuItem item) {
+              switch (item.getItemId()) {
+              case R.id.acercaDe:
+                     lanzarAcercaDe(null);
+                     break;
+                     
+              case R.id.config:
+                  lanzarPreferencias(null);
+                  break;
+              }
+              
+              return true; /** true -> consumimos el item, no se propaga*/
+     }    
+    
+}
---
+++ Entrega1/src/com/example/asteroides/Puntuaciones.java
@@ -0,0 +1,23 @@
+package com.example.asteroides;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class Puntuaciones extends ListActivity {
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.puntuaciones);
+       // setListAdapter(new ArrayAdapter<String>(this, R.layout.elemento_lista, R.id.titulo,  Asteroides.almacen.listaPuntuaciones(10)));
+        
+        setListAdapter(new MiAdaptador(this, Asteroides.almacen.listaPuntuaciones(10)));
+    }
+    
+    @Override protected void onListItemClick(ListView listView, View view, int position, long id) {
+    			super.onListItemClick(listView, view, position, id);
+    			Object o = getListAdapter().getItem(position);
+    			Toast.makeText(this, "Selección: " + Integer.toString(position) +  " - " + o.toString(),Toast.LENGTH_LONG).show();
+    		}
+}
